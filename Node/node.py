02ea12b6470a268790f8deaa21811695f2921cfe
Shaswat Prabhat <spd1238@gmail.com>
2021-08-16 11:06:13 +0530

    Add Basic Node class and Runner for Trees.

---
+++ Node/node.py
@@ -0,0 +1,6 @@
+class NodeClass():
+    def __init__(self, Value):
+        super().__init__()
+        self.Value = Value
+        self.Left = None
+        self.Right = None
---
+++ Runners/runner.py
@@ -0,0 +1,4 @@
+from Node.node import NodeClass
+
+if __name__ == '__main__':
+    NodeClass(42)
